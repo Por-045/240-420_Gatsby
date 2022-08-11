7837b8033b40290fe03d86d2c6809a2619e1f85b
Por-045 <6210110045@psu.ac.th>
2022-08-11 16:42:45 +0700

    query_init

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,29 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    // ... some gatsby plugins
+
+    // You can take advantage of the following plugins with gatsby-source-directus
+
+    // `gatsby-plugin-image`,
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`,
+
+    // Finally our plugin
+    {
+        resolve: '@directus/gatsby-source-directus',
+        options: {
+            url: `https://a1aen8jt.directus.app/`, // Fill with your Directus instance address
+            auth: {
+                token: 'GbsvT8VxIV6K5ugZOY8EKvyEO-gJHIQ0', // You can use a static token from an user
+
+                // Or you can use the credentials of an user
+                // email: "johndoe@directus.cloud",
+                // password: "mysecretpassword",
+            },
+        },
+    },
+],
+
 }
